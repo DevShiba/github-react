0b32e42d503ee8f0fc827965b7ebc22cdfb7db33
DevShiba <migerushiba@gmail.com>
2024-05-08 00:46:12 -0300

    Info Component

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
+  return (
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error, isLoading }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubContext, GithubProvider };
